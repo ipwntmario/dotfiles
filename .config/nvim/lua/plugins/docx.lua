91a7d727d9d0a7ce3c4be61a7e17fa688b77bf39
DylTheShyGuy <nin10dos@gmail.com>
2026-09-08 20:14:30 -0400

    feat: add nvim-docx plugin to neovim

---
+++ .config/nvim/lua/plugins/docx.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    "DaanHessen/nvim-docx",
+    ft = "docx", -- optional: lazy load on docx filetype if supported
+    opts = {}, -- add any configuration options here if needed
+  },
+}
