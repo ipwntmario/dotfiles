8a2b1a5a6d3d8829b22c09fa4b88e2f7c890bc0a
Dylan <nin10dos@gmail.com>
2026-09-19 12:25:02 -0400

    add kanagawa installation as a nvim colorscheme option

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,6 @@
 return {
   -- 1. Download the colorscheme plugin via GitHub repository string
   {
-    -- "rebelot/kanagawa.nvim",
     "folke/tokyonight.nvim",
     lazy = false, -- Make sure the theme loads immediately on startup
     priority = 1000, -- Load the theme before all other plugins
@@ -12,6 +11,12 @@ return {
         floats = "transparent",
       },
     },
+  },
+
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false, -- Make sure the theme loads immediately on startup
+    priority = 1000, -- Load the theme before all other plugins
   },
 
   -- 2. Tell LazyVim to load this colorscheme automatically
